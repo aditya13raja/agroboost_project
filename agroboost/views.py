047b8ce65c02c82d76d9b88df8b574dc3624ac0e
aditya13raja <aditya13raja@gmail.com>
2025-04-21 11:16:52 +0530

    Create necessary views

--- agroboost/views.py
+++ agroboost/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import UserRegisterForm, ProductForm
+from .models import Profile, Product, Resource
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            Profile.objects.create(user=user, role=form.cleaned_data['role'])
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'register.html', {'form': form})
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save(commit=False)
+            product.uploader = request.user
+            product.save()
+            return redirect('dashboard')
+    else:
+        form = ProductForm()
+    return render(request, 'add_product.html', {'form': form})
+
+def view_products(request):
+    products = Product.objects.all()
+    return render(request, 'view_products.html', {'products': products})
