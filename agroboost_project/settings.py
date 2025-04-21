a863e5448489a6b758ee73a3e7adb3e9d1a640fe
aditya13raja <aditya13raja@gmail.com>
2025-04-21 15:14:46 +0530

    Move all templates file to app

--- agroboost_project/settings.py
+++ agroboost_project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'agroboost_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'agroboost' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
